79253b3b1805e46e9542161d7673f9b992d14aa6
anasmasti <anasmasti@hotmail.com>
2021-05-27 14:52:46 +0100

    cleaning code

--- pages/index.vue
+++ pages/index.vue
@@ -6,16 +6,19 @@
     </div>
 
     <Domaine />
+
     <OurService />
 
     <div class="d-flex flex-wrap py-5">
       <div class="col-12 col-lg-6 col-xl-6">
         <Expertise />
       </div>
+
       <div class="col-12 col-lg-6 col-xl-6">
         <Mission />
       </div>
     </div>
+
     <div class="pt-5">
       <Partenaire />
     </div>
@@ -29,6 +32,7 @@
     <div class="line bg_light_2 py-5">
       <hr />
     </div>
+
     <Contactez />
   </div>
 </template>
